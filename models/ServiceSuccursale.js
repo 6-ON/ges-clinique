b9a17f44e0375dcc7b4a6656bf4d266949b7f293
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-12 22:32:26 +0100

    Creation du model ServiceSuccursal

--- models/ServiceSuccursale.js
+++ models/ServiceSuccursale.js
@@ -1,7 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/";
 
-const ServiceSuccursale = sequelize.define("Succursale", {
+export const ServiceSuccursale = sequelize.define("Succursale", {
   
 });
-export default ServiceSuccursale
