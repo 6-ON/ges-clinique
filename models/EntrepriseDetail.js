06a88ee1613eb4453438777add34bf6493d10e40
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-12 22:08:27 +0100

    Creation du model EntrepriseDetail

---
+++ models/EntrepriseDetail.js
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/";
+
+const EntrepriseDetail = sequelize.define("Succursale", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: { type: DataTypes.STRING, allowNull: false },
+  adress: { type: DataTypes.STRING, allowNull: false },
+  
+});
+export default EntrepriseDetail
