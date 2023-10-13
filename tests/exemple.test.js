c1f587d109173c604639f59cf54efb8591b1ef50
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-14 00:16:43 +0100

    setup testing exemple

---
+++ tests/exemple.test.js
@@ -0,0 +1,9 @@
+import request from "supertest";
+import app from "../app";
+describe("GET /", () => {
+	it("should return 200 OK", async () => {
+		const response = await request(app).get("/");
+		expect(response.statusCode).toBe(200);
+		expect(response.text).toBe("Hello World!");
+	});
+});
