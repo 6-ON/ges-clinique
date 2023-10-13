3c563d9809dcf4693474c44a30242a356c66ad61
Laaouina18 <laaouinanouhaila2019@gmail.com>
2023-10-13 09:06:06 +0100

    renommer le model

--- models/Employee.js
+++ models/Employee.js
@@ -2,7 +2,7 @@ import { DataType, DataTypes } from "sequelize";
 import sequelize from "../config";
 
 
-export const Employee=sequelize.define('Employe',{
+export const Employee=sequelize.define('Employee',{
 id:{
     type:DataTypes.INTEGER,
     PrimaryKey:true,
