678b7304845f4db21db49b034acdee4949b45119
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-15 20:59:46 +0100

    Ajouter Port Database

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME , DB_PORT } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
 	dialect: "mysql",
 	host: DB_HOST,
+	port: DB_PORT
 });
 export default sequelize;
