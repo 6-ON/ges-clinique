b4917e8daca7c00a269a11d7cf691caa97443424
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-16 02:23:02 +0100

    add winston for logs

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import logger from "../utils/logger";
 import "dotenv/config";
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME , DB_PORT } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
 	dialect: "mysql",
 	host: DB_HOST,
-	port: DB_PORT
+	port: DB_PORT,
+	logging: (msg) => logger.info(msg),
 });
 export default sequelize;
