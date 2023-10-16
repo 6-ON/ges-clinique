67dcb6ca9a088ef216f982818d046af4de4b8470
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-16 17:33:42 +0100

    add template to ChefController

---
+++ controllers/ClientController.js
@@ -0,0 +1,38 @@
+/**
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+ */
+
+export const ClientController = {
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	index: (req, res) => {
+		
+	},
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	create: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	show: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	update: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	delete: (req, res) => {},
+};
