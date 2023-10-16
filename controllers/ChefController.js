c021e7b30aec6acb12f93beffe3cecc468873bc6
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-16 11:50:55 +0100

    create template for controllers

---
+++ controllers/ChefController.js
@@ -0,0 +1,36 @@
+/**
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Request} Response
+ */
+
+export const ChefController = {
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	index: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	create: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	show: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	update: (req, res) => {},
+	/**
+	 * @param {Request} req
+	 * @param {Request} res
+	 */
+	delete: (req, res) => {},
+};
