118b7d6fc5da8d138712f5544ea67de013d7beb0
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-16 17:34:48 +0100

    test create chef method

--- tests/addChef.test.js
+++ tests/addChef.test.js
@@ -1,9 +1,14 @@
+import app from "../app";
 import { User, Chef } from "../models";
+import logger from "../utils/logger";
+import request from "supertest";
 describe("GET /", () => {
 	it("should add user", async () => {
-		const chef = await Chef.create(
-			{
-				grade: "plus",
+		const response = await request(app)
+			.post("/chefs")
+			.accept("application/json")
+			.send({
+				grade: "haaaa",
 				user: {
 					name: "John Doe",
 					email: "john.doe@example.com",
@@ -11,11 +16,9 @@ describe("GET /", () => {
 					phone: "1234567890",
 					image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2FHD%2520wallpaper%2F&psig=AOvVaw0Q4X6Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4&ust=1629787226268000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQ4Z2Z3_QCFQAAAAAdAAAAABAD",
 				},
-			},
-			{
-				include: [Chef.User],
-			},
-		);
-		console.log(chef);
-	});
+			});
+		
+		logger.info(response.body);
+		logger.info(response.status);
+	}, 1000000);
 });
