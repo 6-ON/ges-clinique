3eefd53349d40281c6e3178046686e62e34d5079
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-16 02:21:33 +0100

    add relations and format file

--- models/associations.js
+++ models/associations.js
@@ -43,36 +43,49 @@ User.Client = User.belongsTo(Client, {
 });
 
 //------------------- Client-Reservation ------------------
-Client.Reservation = Client.hasMany(Reservation,{as:"reservations",foreignKey:"clientId"})
-Reservation.Client = Reservation.belongsTo(Client,{as:"client"})
+Client.Reservation = Client.hasMany(Reservation, { as: "reservations", foreignKey: "clientId" });
+Reservation.Client = Reservation.belongsTo(Client, { as: "client" });
 
 //------------------- Client-EntrepriseDetail ------------------
-Client.EntrepriseDetail = Client.hasOne(EntrepriseDetail,{as:"entrepriseDetail"})
-EntrepriseDetail.Client = EntrepriseDetail.belongsTo(Client)
+Client.EntrepriseDetail = Client.hasOne(EntrepriseDetail, { as: "entrepriseDetail" });
+EntrepriseDetail.Client = EntrepriseDetail.belongsTo(Client);
 
 //------------------- Client-Employee ------------------
-Employee.Client=Employee.belongsTo(Client,{as:"client",foreignKey:"clientId"})
-Client.Employee=Client.hasMany(Employee,{as:"employee",foreignKey:"clientId"})
+Employee.Client = Employee.belongsTo(Client, { as: "client", foreignKey: "clientId" });
+Client.Employee = Client.hasMany(Employee, { as: "employees", foreignKey: "clientId" });
 //------------------- Service-ExigenceService ------------------
-Service.ExigenceService = Service.hasMany(ExigenceService,{as:"exigenceService",foreignKey:"serviceId"})
-ExigenceService.Service = ExigenceService.belongsTo(Service,{foreignKey:"serviceId"})
+Service.ExigenceService = Service.hasMany(ExigenceService, { as: "exigenceService", foreignKey: "serviceId" });
+ExigenceService.Service = ExigenceService.belongsTo(Service, { foreignKey: "serviceId" });
 //------------------- Service-Succursale ------------------
-Succursale.Service = Succursale.belongsToMany(Service,{as:"services",through:ServiceSuccursale})
-Service.Succursale = Service.belongsToMany(Succursale,{as:"succursales",through:ServiceSuccursale})
+Succursale.Service = Succursale.belongsToMany(Service, { as: "services", through: ServiceSuccursale });
+Service.Succursale = Service.belongsToMany(Succursale, { as: "succursales", through: ServiceSuccursale });
 //------------------- Service-Reservation ------------------
-
+Service.Reservation = Service.hasMany(Reservation, { as: "reservations", foreignKey: "serviceId" });
+Reservation.Service = Reservation.belongsTo(Service, { as: "service" });
 //------------------- Reservation-Facture ------------------
-Reservation.Facture=Reservation.hasOne(Facture,{as:"facture"})
-Facture.Reservation=Facture.belongsTo(Reservation,{as:"reservation"})
+Reservation.Facture = Reservation.hasOne(Facture, { as: "facture" });
+Facture.Reservation = Facture.belongsTo(Reservation, { as: "reservation" });
 //------------------- Reclamation-User ------------------
-Reclamation.User=Reclamation.belongsTo(User,{as:"user",foreignKey:"userableId"})
-User.Reclamation=User.hasMany(Reclamation,{as:"reclamation",foreignKey:"userableId"})
+Reclamation.User=Reclamation.belongsTo(User,{as:"user",foreignKey:"userId"})
+User.Reclamation=User.hasMany(Reclamation,{as:"reclamations",foreignKey:"userId"})
 //------------------- Reclamation-Technicien ------------------
-Technicien.Reclamation = Technicien.hasMany(Reclamation,{as:"reclamation",foreignKey:"technicienId"})
-Reclamation.Technicien = Reclamation.belongsTo(Technicien,{as:"technicien"})
+Technicien.Reclamation = Technicien.hasMany(Reclamation, { as: "reclamations", foreignKey: "technicienId" });
+Reclamation.Technicien = Reclamation.belongsTo(Technicien, { as: "technicien" });
 //------------------- Technicien-Reservation ------------------
-Technicien.Reservation=Technicien.hasMany(Reservation,{as:"reservation",foreignKey:"techniciensId"})
-Reclamation.Technicien=Reclamation.belongsTo(Technicien,{as:"technicien",foreignKey:"techniciensId"})
+Technicien.Reservation = Technicien.hasMany(Reservation, { as: "reservations", foreignKey: "technicienId" });
+Reservation.Technicien = Reservation.belongsTo(Technicien, { as: "technicien", foreignKey: "technicienId" });
 //------------------- Technicien-Succursale ------------------
-
+Technicien.Succursale = Technicien.belongsTo(Succursale, { as: "succursale", foreignKey: "succursaleId" });
+Succursale.Technicien = Succursale.hasMany(Technicien, { as: "techniciens", foreignKey: "succursaleId" });
 //------------------- Technicien-User ------------------
+Technicien.User = Technicien.hasOne(User, {
+	foreignKey: "userableId",
+	constraints: false,
+	scope: { userableType: "Technicien" },
+	as: "user",
+});
+User.Technicien = User.belongsTo(Technicien, {
+	foreignKey: "userableId",
+	constraints: false,
+	as: "technicien",
+});
