4694a5d4c4064f695d49ab10c790faafa062fc91
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-18 18:21:10 +0100

    test with faker

--- tests/addChef.test.js
+++ tests/addChef.test.js
@@ -1,23 +1,23 @@
 import app from "../app";
-import { User, Chef } from "../models";
 import logger from "../utils/logger";
 import request from "supertest";
+import { faker } from "@faker-js/faker";
+import { hash } from "argon2";
+
 describe("GET /", () => {
-	it("should add user", async () => {
-		const response = await request(app)
-			.post("/chefs")
-			.accept("application/json")
-			.send({
-				grade: "haaaa",
-				user: {
-					name: "John Doe",
-					email: "john.doe@example.com",
-					password: "password123",
-					phone: "1234567890",
-					image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2FHD%2520wallpaper%2F&psig=AOvVaw0Q4X6Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4&ust=1629787226268000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQ4Z2Z3_QCFQAAAAAdAAAAABAD",
-				},
-			});
-		
+	it("should add chef and user", async () => {
+		const chefData = {
+			grade: faker.string.alpha({ casing: "lower" }),
+			user: {
+				name: faker.person.firstName(),
+				email: faker.internet.email(),
+				password: await hash(faker.internet.password()),
+				phone: faker.phone.number(),
+				image: faker.image.urlLoremFlickr({ width: 640, height: 480, category: "human" }),
+			},
+		};
+		const response = await request(app).post("/chefs").accept("application/json").send(chefData);
+
 		logger.info(response.body);
 		logger.info(response.status);
 	}, 1000000);
