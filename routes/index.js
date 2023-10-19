e4f3ff320464c047b356e4bd71db74ae4a40fa6c
Laaouina18 <laaouinanouhaila2019@gmail.com>
2023-10-19 17:54:34 +0100

    exclude files from revert

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,18 @@ import chefRoutes from "./ChefRoutes";
 import AuthRoutes from "./AuthRoutes";
 import technicienRoutes from "./TechnicienRoutes";
 import serviceRoutes from "./ServiceRoutes";
-import succursaleRoutes from "./SuccursaleRoutes";
 import factureRoutes from "./FactureRoutes";
+import succursaleRoutes from "./SuccursaleRoutes";
+
 const router = Router();
 router.use(json())
 
 router.use("/chefs", chefRoutes);
 router.use("/auth", AuthRoutes)
 router.use("/services", serviceRoutes);
+router.use("/factures",factureRoutes);
+router.use("/succursales",succursaleRoutes);
 router.use("/techniciens", technicienRoutes);
-router.use("/succursales", succursaleRoutes);
-router.use("/factures", factureRoutes);
 
 
 
