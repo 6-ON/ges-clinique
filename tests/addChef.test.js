886831ccf43c3b69a7970312fe566ae038e80a5f
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-19 15:05:34 +0100

    Revert "Client Controller + test index,show,create,delete"
    This reverts commit 9008bf076c267c450a2f7ba30e1d5acb90f938b4.

--- tests/addChef.test.js
+++ tests/addChef.test.js
@@ -8,10 +8,10 @@ describe("GET /", () => {
 			.post("/chefs")
 			.accept("application/json")
 			.send({
-				grade: "slhlsl",
+				grade: "haaaa",
 				user: {
-					name: "sdish Doe",
-					email: "sdlhs.dokle@example.com",
+					name: "John Doe",
+					email: "john.doe@example.com",
 					password: "password123",
 					phone: "1234567890",
 					image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2FHD%2520wallpaper%2F&psig=AOvVaw0Q4X6Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4Z2Z3Q4Q4&ust=1629787226268000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQ4Z2Z3_QCFQAAAAAdAAAAABAD",
