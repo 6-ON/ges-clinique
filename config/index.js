e3eeade9c6dff590fbe6c71d2c1493d466c82fe8
Amine Hatim <60559161+6-ON@users.noreply.github.com>
2023-10-21 17:13:04 +0100

    add charset

--- config/index.js
+++ config/index.js
@@ -4,8 +4,10 @@ import "dotenv/config";
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, SEQUELIZE_LOG } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
 	dialect: "mysql",
+	charset: "utf8",
 	host: DB_HOST,
 	port: DB_PORT,
 	logging: +SEQUELIZE_LOG ? (msg) => logger.info(msg) : false,
+
 });
 export default sequelize;
