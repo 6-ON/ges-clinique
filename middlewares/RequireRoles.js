4e6eea6b4cddfc564808dcb2d039b73649327c81
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-22 18:32:26 +0100

    move auth logic to authenticate middleware

--- middlewares/RequireRoles.js
+++ middlewares/RequireRoles.js
@@ -3,7 +3,6 @@
  * @typedef {import("express").Response} Response
  */
 
-import { verifyToken } from "../utils/jwt";
 /**
  * @param {Array<string>} roles 
  */
@@ -14,19 +13,8 @@ export const RequireRoles = (roles) => {
 	 * @param {import("express").NextFunction} next
 	 */
 	return async (req, res, next) => {
-		// get authorization header
-		const authHeader = req.headers.authorization;
-		// check if authorization header is present
-		if (!authHeader) return res.status(401).json("Unauthorized");
-		// check if authorization header is valid
-		const token = authHeader.split(" ")[1];
-		if (!token) return res.status(401).json("Unauthorized");
-		// verify token
-		const decoded = await verifyToken(token);
-		if (!decoded) return res.status(401).json("Unauthorized");
-		if (roles.includes(decoded.role.toLowerCase())) return res.status(403).json("Forbidden");
-		//include User.Chef or User.Technicien or User.Client basel on role
-		req.user = decoded;
+
+		if (roles.includes(req.user.role.toLowerCase())) return res.status(403).json("Forbidden");
 		next();
 	};
 };
