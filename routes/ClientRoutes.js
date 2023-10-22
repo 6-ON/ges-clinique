937284e49028efcf7720296efc6eb6f1ccf9bac2
Karimcherraoui <cherraoui.karim96@gmail.com>
2023-10-22 18:32:40 +0100

    add update route

--- routes/ClientRoutes.js
+++ routes/ClientRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { ClientController } from "../controllers";
+import {ClientController} from "../controllers";
 const router = Router();
 
 
 router.get("/", ClientController.index);
 router.get("/:id", ClientController.show);
 router.post("/", ClientController.create);
+router.put("/:id", ClientController.update);
 router.delete("/:id", ClientController.delete);
 
 export default router; 
